95b2ac3e5e7c9d7a94cad4da6803a5f9bac2eefb
TECHNICAL\konstantagkas <konstantagkas@intralot.com>
2021-06-30 16:47:51 +0300

    changed main branch with promises

--- controller/customerController.js
+++ controller/customerController.js
@@ -1,5 +1,27 @@
 const Customer = require("../model/customer.model.js");
 
+// Retrieve all Customers from the database.
+exports.findAll = (req, res) => {
+  Customer.getAll()
+    .then((data) => res.status(200).send(data))
+    .catch((err) => res.status(500).send(err));
+};
+
+// Find a customer from the database.
+exports.findOne = (req, res) => {
+  Customer.findById(req.params.customerId)
+    .then((data) => {
+      if (data === undefined) {
+        res
+          .status(404)
+          .send(`customer with id:${req.params.customerId} not found`);
+      } else {
+        res.status(200).send(data);
+      }
+    })
+    .catch((err) => res.status(500).send(err));
+};
+
 // Create and Save a new Customer
 exports.create = (req, res) => {
   if (!req.body) {
@@ -12,47 +34,9 @@ exports.create = (req, res) => {
     active: req.body.active,
   });
 
-  Customer.create(customer, (err, data) => {
-    if (err) {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Customer.",
-      });
-    } else {
-      res.send(data);
-    }
-  });
-};
-
-// Retrieve all Customers from the database.
-exports.findAll = (req, res) => {
-  Customer.getAll((err, data) => {
-    if (err) {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving customers.",
-      });
-    } else {
-      res.send(data);
-    }
-  });
-};
-
-// Find a customer from the database.
-exports.findOne = (req, res) => {
-  Customer.findById(req.params.customerId, (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found Customer with id ${req.params.customerId}.`,
-        });
-      } else {
-        res.status(500).send({
-          message: "Error retrieving Customer with id " + req.params.customerId,
-        });
-      }
-    } else res.send(data);
-  });
+  Customer.create(customer)
+    .then((data) => res.status(201).send(data))
+    .catch((err) => res.status(500).send(err));
 };
 
 // Update a Customer identified by the customerId in the request
@@ -63,52 +47,27 @@ exports.update = (req, res) => {
     });
   }
 
-  Customer.updateById(
-    req.params.customerId,
-    new Customer(req.body),
-    (err, data) => {
-      if (err) {
-        if (err.kind === "not_found") {
-          res.status(404).send({
-            message: `Not found Customer with id:${req.params.customerId}.`,
-          });
-        } else {
-          res.status(500).send({
-            message: `Error updating Customer with id:${req.params.customerId}`,
-          });
-        }
-      } else {
-        res.send(data);
-      }
-    }
-  );
+  Customer.updateById(req.params.customerId, new Customer(req.body))
+    .then((data) => {
+      res.status(201).send(data);
+    })
+    .catch((err) => res.status(500).send(err));
 };
 
 // Delete a Customer with the specified customerId in the request
 exports.delete = (req, res) => {
-  Customer.remove(req.params.customerId, (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found Customer with id ${req.params.customerId}.`,
-        });
-      } else {
-        res.status(500).send({
-          message: "Could not delete Customer with id " + req.params.customerId,
-        });
-      }
-    } else res.send({ message: `Customer was deleted successfully!` });
-  });
+  Customer.remove(req.params.customerId)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => res.status(500).send(err));
 };
 
 // Delete all Customers from the database.
 exports.deleteAll = (req, res) => {
-  Customer.removeAll((err, data) => {
-    if (err)
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while removing all customers.",
-      });
-    else res.send({ message: `All Customers were deleted successfully!` });
-  });
+  Customer.removeAll()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => res.status(500).send(err));
 };
